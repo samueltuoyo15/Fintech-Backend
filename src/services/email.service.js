a399ee619e955580865c548f7a6d9fd9bed0a5f7
samueltuoyo15 <samueltuoyo9082@gmail.com>
2025-07-11 11:54:00 +0100

    updated smtp details

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -14,7 +14,6 @@ const sendEmailVerification = async (email) => {
     const transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST,
       port: Number(process.env.SMTP_PORT),
-      secure: Number(process.env.SMTP_PORT) === 465,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
