1d8f6c46f9a7c13834b8b74167401855c3f5338f
samueltuoyo15 <samueltuoyo9082@gmail.com>
2025-07-12 12:31:35 +0100

    updated origins

--- src/server.js
+++ src/server.js
@@ -21,9 +21,16 @@ const allowedOrigins = [
 ]
 
 app.use(cors({
-  origin: "*",
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"]
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }))
 
 app.use(requestLogger)
