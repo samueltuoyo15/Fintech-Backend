08582bbb3b00a1ac250c253452570b0c3fc43bb2
samueltuoyo15 <samueltuoyo9082@gmail.com>
2025-07-12 12:06:36 +0100

    auth route

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -6,7 +6,7 @@ import { authenticateUser } from "../middleware/auth.middleware.js"
 const router = Router()
 
 router.post("/register", validateSignUpInput, registerUser)
-router.post("/verify-email", verifyUserEmail)
+router.get("/verify-email", verifyUserEmail)
 router.post("/login", validateLoginInput, loginUser)
 router.post("/logout", authenticateUser, logoutUser)
 router.post("/refresh-token", refreshTokenController)
