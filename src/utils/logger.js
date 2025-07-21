2f41d2f4d7ee32789c38be23b328a3c0a30090e1
Samuel Tuoyo <samueltuoyo9082@gmail.com>
2025-07-21 17:02:50 +0100

    Update logger.js

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,7 +1,7 @@
 import pino from "pino"
 
 const logger = pino({
-    level: process.env.NODE_ENV === "productino" ? "info" : "debug",
+    level: process.env.NODE_ENV === "production" ? "info" : "debug",
     ...process.env.NODE_ENV !== "production" && {
         transport: {
             target: "pino-pretty",
