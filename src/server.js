4fb3734c0eb7b37d4e64c0c6a9b86104e44c62fe
Samuel Tuoyo <samueltuoyo9082@gmail.com>
2025-08-26 17:48:35 +0100

    Fear: add production domain to cors array

--- src/server.js
+++ src/server.js
@@ -19,7 +19,8 @@ const app = express()
 
 const allowedOrigins = [
   "http://localhost:3000",
-  "https://ife-global-jkv6.vercel.app" 
+  "https://ife-global-jkv6.vercel.app",
+  "https://ife-elroiglobal.com"
 ]
 
 app.use(cors({
